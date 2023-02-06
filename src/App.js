809aa6d97ead0d979a0f84b759f3048b134359fa
ManagementSystemDeveloper <edomkyc777@gmail.com>
2023-02-07 03:48:23 +0900

    Resolve "Project Structure"

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import './config/ReactotronConfig';
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
+import store from './store/index';
+import history from './services/history';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+        <Header />
+        <Routes />
+        <ToastContainer autoClose={3000} />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
